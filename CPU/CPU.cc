43bf7e8fb9a926dc8f0b7f6302244b2592174449
William Silveira <williamsilveiraf@hotmail.com>
2024-04-19 14:49:28 -0300

    feat: CPU implementation done

--- CPU/CPU.cc
+++ CPU/CPU.cc
@@ -12,9 +12,35 @@ CPU::CPU() {
     for (int i = 0; i < 6; ++i) registrars[i] = 0;
 }
 
+/**
+ * loadContextCursor
+ * Carrego o contexto na CPU durante o chaveamento entre
+ * o processo que está em execução e o próximo processo
+ * a ser executado
+*/
+void CPU::loadContextCursor(const Context& context_instance) {
+    STACK_POINTER = context_instance.STACK_POINTER;
+    PROGRAM_COUNTER = context_instance.PROGRAM_COUNTER;
+    STATUS = context_instance.STATUS;
 
+    for (int i = 0; i < 6; ++i) registrars[i] = context_instance.registrars[i];
+}
 
+/**
+ * fetchContextCursor
+ * Busco o contexto atual do processo em execução na CPU 
+ * Uso para salvar o estado atual da CPU antes do chaveamento do processo
+*/
+CPU::Context CPU::fetchContextCursor() const {
+    Context cursor_context;
+    cursor_context.STACK_POINTER = STACK_POINTER;
+    cursor_context.PROGRAM_COUNTER = PROGRAM_COUNTER;
+    cursor_context.STATUS = STATUS;
 
+    for (int i = 0; i < 6; ++i) cursor_context.registrars[i] = registrars[i];
+    
+    return cursor_context;
+} 
 /**
  * Contexto da CPU
  * Inicializo todos os registradores, stack pointer, program counter e status
